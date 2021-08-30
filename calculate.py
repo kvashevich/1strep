854d83ff198c8bebf122254ebea57aa6de14c873
Mikhail <mishakvashevich14gmail.com>
2021-08-31 02:59:57 +0300

    done

--- calculate.py
+++ calculate.py
@@ -1,1 +1,46 @@
-111111111111111
+answer = True
+while answer is True:
+    while True:
+        try:
+            first_number = float(input("Enter first number --> "))
+            break
+        except ValueError:
+            print("Wrong data!")
+            continue
+
+    while True:
+        operation_list = ["+", "-", "/", "*"]
+        operation = input("Choose the operation(+, -, /, *) --> ")
+        if operation not in operation_list:
+            print("Wrong operation!")
+            continue
+        break
+
+    while True:
+        try:
+            second_number = float(input("Enter second number --> "))
+            break
+        except ValueError:
+            print("Wrong data!")
+            continue
+
+    operation_dict = {
+        "+": first_number + second_number,
+        "-": first_number - second_number,
+        "/": first_number / second_number,
+        "*": first_number * second_number,
+    }
+
+    print(f"\n{first_number} {operation} {second_number} = {operation_dict[operation]}")
+
+    while True:
+        continue_ = input("\nWould you like repeat operation?(y/n): ")
+        if continue_ == "y" or continue_ == "n":
+            if continue_ == "y":
+                break
+            else:
+                print("\nGood bye!")
+                answer = False
+                break
+        else:
+            print("Wrong answer!")
